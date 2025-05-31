c58a82599774822b72ac2b0baba087deee344ad6
JsonLevine <50605966+JsonLevine@users.noreply.github.com>
2025-05-31 14:44:24 -0400

    Initial commit: Creating project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Footer() {
+  
+  return (
+    <>
+      <div className="flex flex-row items-center justify-center text-center md:p-10 p-2">
+        <a 
+          data-testid="footer-link" 
+          className="flex flex-col items-center justify-center transition duration-300 ease-in-out hover:text-stronghold-green hover:-translate-y-1" 
+          href="https://github.com/JsonLevine/jason-portfolio" 
+          target="_blank" 
+          rel="noreferrer">
+          <span>
+            <span className="text-stronghold-red">2025</span> Designed and built by  
+            <span className="jersey text-stronghold-red pl-1">
+              <strong>Jason Levine</strong><br/>
+            </span>
+          </span>
+          <span className="text-sm">with <strong className="text-stronghold-platinum">React/Vite</strong> + <strong className="text-stronghold-platinum">TailwindCSS</strong></span>
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default Footer
---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        fadeIn:{
+          '0%':{opacity: '0'},
+          '100%':{opacity: '1'}
+        }
+      },
+      colors: {
+        'stronghold-red': '#E64747',
+        'stronghold-eerie-black': '#191919',
+        'stronghold-onyx': '#404040',
+        'stronghold-platinum': '#E4E4E4',
+        'stronghold-jet': '#343434',
+        'stronghold-white': '#ffffff',
+        'stronghold-green': '#4ADE80',
+        'chewy-blue': '#3b82f6',
+        'optum-orange': '#f97316',
+        'link-blue': '#818cf8',
+        'main-text': '#9CA3AF'
+      },
+      boxShadow: {
+        'project-shadow': '10px 10px 0 0 rgba(230, 71, 71, 0.5)',
+      },
+      animation: {
+        fadeIn: 'fadeIn 1.5s ease-in',
+        'spin-slow': 'spin 3s linear infinite',
+      },
+    },
+  },
+  plugins: [
+    require('tailwindcss-animated')
+  ],
+}
