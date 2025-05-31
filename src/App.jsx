002bf1457ada56e493c0d2028fff1bd1a316078f
JsonLevine <50605966+JsonLevine@users.noreply.github.com>
2025-05-31 15:42:14 -0400

    Footer and favicons

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import Footer from './components/Footer.jsx'
+import Main from './components/Main.jsx'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <h1>Job Tracker Application</h1>
-    </>
+      <Main />
+      <Footer />
+    </div>
   )
 }
 
