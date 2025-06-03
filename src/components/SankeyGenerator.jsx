9b528c3375048ea71a78f2fa27b3a95b5171c29d
JsonLevine <50605966+JsonLevine@users.noreply.github.com>
2025-06-02 21:28:15 -0400

    Begin Sankey Generator

--- src/components/SankeyGenerator.jsx
+++ src/components/SankeyGenerator.jsx
@@ -6,9 +6,46 @@ function SankeyGenerator() {
   return (
     <div className="flex-1 ring-2 rounded ring-stronghold-red text-stronghold-red jersey flex flex-col items-center justify-start p-4 mt-8">
       <h2>Sankey Generator</h2>
+      <form className="flex flex-col items-center justify-center w-full">
+        <input
+          type="text"
+          placeholder="Enter number of applications"
+          className="mb-4 w-full p-2 border rounded"
+          value={data.applications || ''}
+          onChange={(e) => setData({ ...data, applications: e.target.value })}
+        />
+        <label className="mb-2">
+          Data:
+          <textarea
+            className="w-full h-32 p-2 border rounded"
+            value={JSON.stringify(data, null, 2)}
+            onChange={(e) => setData(JSON.parse(e.target.value))}
+          />
+        </label>
+        <button
+          type="submit"
+          className="px-4 py-2 bg-stronghold-red text-white rounded hover:bg-stronghold-red-dark"
+        >
+          Generate Sankey Diagram
+        </button>
+      </form>
       <div></div>
     </div>
   )
 }
 
 export default SankeyGenerator
+
+// Sample Job Search diagram:
+
+// Applications [4] 1st Interviews
+// Applications [9] Rejected
+// Applications [4] No Answer
+
+// 1st Interviews [2] 2nd Interviews
+// 1st Interviews [2] No Offer
+
+// 2nd Interviews [2] Offers
+
+// Offers [1] Accepted
+// Offers [1] Declined
