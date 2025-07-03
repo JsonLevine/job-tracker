96bc12ad0eeb07a583fc10102eb194934bb95572
JsonLevine <50605966+JsonLevine@users.noreply.github.com>
2025-07-03 19:31:25 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,15 +13,18 @@ module.exports = {
       },
       colors: {
         'stronghold-red': '#E64747',
+        'stronghold-red-accessible': '#F84D4D',
         'stronghold-eerie-black': '#191919',
         'stronghold-onyx': '#404040',
         'stronghold-platinum': '#E4E4E4',
         'stronghold-jet': '#343434',
-        'stronghold-white': '#ffffff',
+        'stronghold-white': '#FFFFFF',
         'stronghold-green': '#4ADE80',
-        'chewy-blue': '#3b82f6',
-        'optum-orange': '#f97316',
-        'link-blue': '#818cf8',
+        'chewy-blue': '#3B82F6',
+        'chewy-blue-accessible': '#99C0FF',
+        'optum-orange': '#F97316',
+        'optum-orange-accessible': '#F99550',
+        'link-blue': '#8F99FF',
         'main-text': '#9CA3AF'
       },
       boxShadow: {
