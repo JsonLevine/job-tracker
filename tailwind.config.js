8e955cb0fa2bdf83fca17ecd79f4c6c1f05e5aaa
JsonLevine <50605966+JsonLevine@users.noreply.github.com>
2025-08-02 11:57:58 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,14 +26,9 @@ module.exports = {
       boxShadow: {
         'project-shadow': '10px 10px 0 0 rgba(230, 71, 71, 0.5)',
       },
-      animation: {
-        fadeIn: 'fadeIn 1.5s ease-in',
-        'spin-slow': 'spin 3s linear infinite',
-      },
     },
   },
   plugins: [
-    require('tailwindcss-animated'),
     require('@tailwindcss/forms'),
   ],
 }
